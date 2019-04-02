353f68d65b600ea401d3a074141d100f043dfd38
KraftKatten <kraftj@student.chalmers.se>
2019-04-02 15:10:16 +0200

    Changed config to 3D

--- BoidSim/BoidSim/boid.h
+++ BoidSim/BoidSim/boid.h
@@ -8,7 +8,7 @@ struct Boid {
 	glm::vec3 position, velocity;
 
 	Boid()
-		: position(rand() % 161 - 80, rand() % 161 - 80, 0), velocity(rand() % 161 - 80, rand() % 161 - 80, 0) { }
+		: position(rand() % 161 - 80, rand() % 161 - 80, rand() % 81 - 40 ), velocity(rand() % 161 - 80, rand() % 161 - 80, rand() % 81 - 40) { }
 };
 
 #endif
