82e50b05ecd280a64d476a4ac5872ccf56eee65d
oskery <oskar.lyrstrand@gmail.com>
2019-04-03 21:55:13 +0200

    Created function for drawing weapon

--- BoidSim/BoidSim/Main.cpp
+++ BoidSim/BoidSim/Main.cpp
@@ -132,6 +132,30 @@ void drawCrosshair() {
 	glBindVertexArray(0);
 }
 
+void drawWeapon() {
+	float vertices[] = {
+		// position hand     // texture coords
+		0.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+		1.0f,  0.0f,  0.0f,	 1.0f, 0.0f,
+	    1.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+		0.0f, -1.0f,  0.0f,  0.0f, 1.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glDrawArrays(GL_LINES, 0, 8);
+
+	// unbind buffer and vertex array
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
 
 int main()
 {
