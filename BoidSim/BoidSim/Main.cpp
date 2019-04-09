19062ca624c1280ecfc5f96a59ea1fd3335647d2
oskery <oskar.lyrstrand@gmail.com>
2019-04-09 08:54:04 +0200

    Render text on screen with FreeType

--- BoidSim/BoidSim/Main.cpp
+++ BoidSim/BoidSim/Main.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <vector>
+#include <map>
 #include "Shader.h"
 #include <list>
 #include "boid.h"
@@ -14,6 +15,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <iostream>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H  
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -44,6 +48,76 @@ int nrFrames = 0;
 unsigned int VAO, VBO, EBO, tex1, tex2;
 int nrTextures = 0;
 
+// Score
+int score = 0;
+
+
+// Create a struct for characters
+struct Character {
+	GLuint     TextureID;  // ID handle of the glyph texture
+	glm::ivec2 Size;       // Size of glyph
+	glm::ivec2 Bearing;    // Offset from baseline to left/top of glyph
+	GLuint     Advance;    // Offset to advance to next glyph
+};
+
+// A hashmap storing characters so we only have to instantiate each char once
+std::map<GLchar, Character> Characters;
+
+void loadChars() {
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/Anton.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// Load character glyph 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// Set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// Now store character for later use
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+}
 
 // Reference: http://www.opengl-tutorial.org/miscellaneous/an-fps-counter/
 void printPerformance() {
@@ -113,7 +187,7 @@ void updateBoids(Boid & b) { // Flocking rules are implemented here
 	b.velocity = speed*glm::normalize(newVel);
 }
 
-void drawCrosshair() {
+void renderCrosshair() {
 	float vertices[] = {
 		// position hand     // texture coords
 	   -0.05f*screenHeight / screenWidth,-0.05f,  0.0f,  0.0f, 0.0f,
@@ -148,7 +222,7 @@ void drawCrosshair() {
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
-void drawFire() {
+void renderLaser() {
 	float vertices[] = {
 		// positions         // color
 		0.6f,  -0.2f, 0.0f,  1.0f, 0.0f, 0.0f,
@@ -177,7 +251,7 @@ void drawFire() {
 
 }
 
-void drawWeapon() {
+void renderWeapon() {
 	float vertices[] = {
 		// position hand     // texture coords
 		0.1f,  0.0f,  0.0f,  0.0f, 0.0f,
@@ -212,6 +286,54 @@ void drawWeapon() {
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
+void renderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glEnableVertexAttribArray(0);
+
+	// Activate corresponding render state	
+	s.use();
+	s.setVec3("textColor", color);
+	s.setMatrix("projection", glm::ortho(0.0f, (float)screenWidth, 0.0f, (float)screenHeight));
+
+	// Iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		GLfloat xpos = x + ch.Bearing.x * scale;
+		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		GLfloat w = ch.Size.x * scale;
+		GLfloat h = ch.Size.y * scale;
+		// Update VBO for each character
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+		// Render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// Update content of VBO memory
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		// Render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+}
+
+// Used to create textures from images (e.g. rifle and crosshair)
 void createTexture(unsigned int &ref, const char* path) {
 	// Bind texture for gun
 	glBindTexture(GL_TEXTURE_2D, ref);
@@ -226,8 +348,6 @@ void createTexture(unsigned int &ref, const char* path) {
 	int width, height, nrChannels;
 	unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	stbi_image_free(data);
 }
@@ -258,7 +378,10 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
+
+	// Load characters for text to hashmap
+	loadChars();
+
 	// create the boids (allocate space and randomize position/velocity by calling constructor)
 	for (int i = 0; i < nrBoids; ++i)
 		boids.push_back(Boid());
@@ -273,15 +396,21 @@ int main()
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 	glGenTextures(1, &tex1);
+	glGenTextures(1, &tex2);
 		
 	// Build and compile shaders
 	Shader shader("vert.shader", "frag.shader");
 	Shader guiShader("gui.shader", "frag.shader");
 	Shader guiShader2("gui.shader", "gui.frag");
+	Shader textShader("text.vert", "text.frag");
 
-	// Create textures
+	// Create textures from images
 	createTexture(tex1, "rifle.png");
 	createTexture(tex2, "crosshair.png");
+
+	// Enable alpha channel (transparancy) for textures
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// use (bind) the shader 1 so that we can attach matrices
 	shader.use();
@@ -366,12 +495,14 @@ int main()
 		int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
 		state = glfwGetKey(window, GLFW_KEY_SPACE);
 		if (state == GLFW_PRESS) {
-			drawFire();
+			renderLaser();
 		}
 
 		guiShader2.use();
-		drawWeapon();
-		drawCrosshair();
+		renderWeapon();
+		renderCrosshair();
+
+		renderText(textShader, std::to_string(score), 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
