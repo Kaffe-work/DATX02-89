7fe1e9a56940420561c831423822c694fcbfcf0b
oskery <oskar.lyrstrand@gmail.com>
2019-04-10 10:59:17 +0200

    Added function to load cubemap

--- BoidSim/BoidSim/Main.cpp
+++ BoidSim/BoidSim/Main.cpp
@@ -104,6 +104,30 @@ void updateBoids(Boid & b) { // Flocking rules are implemented here
 	b.velocity = speed*glm::normalize(newVel);
 }
 
+void setupCubemap() {
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	int width, height, nrChannels;
+	unsigned char *data;
+	for (GLuint i = 0; i < textures_faces.size(); i++)
+	{
+		data = stbi_load(textures_faces[i].c_str(), &width, &height, &nrChannels, 0);
+		glTexImage2D(
+			GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+			0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+		);
+	}
+
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+}
+
+
 int main()
 {
 	// glfw: initialize and configure
@@ -133,6 +157,7 @@ int main()
 
 	// build and compile shader program
 	Shader shader("vert.shader", "frag.shader");
+	Shader shader("cube.vert", "cube.frag");
 
 	// create the boids (allocate space and randomize position/velocity by calling constructor)
 	for (int i = 0; i < nrBoids; ++i)
