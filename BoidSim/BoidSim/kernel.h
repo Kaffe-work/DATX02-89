848aabe05456e4da8b6e261440a22e1a24b274ca
oskery <oskar.lyrstrand@gmail.com>
2019-05-12 14:12:18 +0200

    Added necessary files

---
+++ BoidSim/BoidSim/kernel.h
@@ -0,0 +1,49 @@
+#ifndef kernel_h
+#define kernel_h
+
+#include "boid.h"
+
+#define NR_BOIDS 100000
+
+#define MAX_COORD 700.f // World boundaries, tested at 700.f
+#define CELL_SIZE 10.f // The world is divided in cubic cells  
+#define BOID_SCOPE 10.f // this is how far boids look for neighbours. Should always be == CELL_SIZE ?
+
+// Boid attributes
+#define PLANE_SOFTNESS 0.5f
+#define DEATH_DISTANCE 3.5f
+#define MAX_ACCELERATION 0.07f
+#define MAX_ACCELERATION_PREDATOR 0.05f
+#define SEPARATION_SOFTNESS 2.0f
+
+#define SEPARATION_FACTOR .05f
+#define ALIGN_FACTOR 0.3f
+//#define COHESION_FACTOR 0.003f // this is the tested value
+#define COHESION_FACTOR 0.1f // but this looks better
+
+#define AVOID_FACTOR 2.0f
+// #define AVOID_DISTANCE 20.0f
+#define PRED_ATTRACT_FACTOR 2.0f
+
+#define MIN_SPEED 0.2f
+#define MAX_SPEED 0.3f
+
+
+// Just a declaration, the actual struct is buried in the .obj file
+struct cudaGraphicsResource;
+
+Boid** initBoidsOnGPU(Boid*);
+void deinitBoidsOnGPU(void);
+void step();
+void prepareBoidRender(Boid* boids, glm::vec3* renderBoids, glm::mat4 projection, glm::mat4 view);
+void printCUDAError();
+
+
+void cudaGraphicsGLRegisterBufferWrapper( struct cudaGraphicsResource** positionsVBO_CUDA , unsigned int positionsVBO);
+void mapBufferObjectCuda( struct cudaGraphicsResource** positionsVBO_CUDA, size_t* num_bytes, glm::vec3** positions);
+void cudaGraphicsUnmapResourcesWrapper(struct cudaGraphicsResource** positionsVBO_CUDA);
+void cudaGraphicsUnregisterResourceWrapper(struct cudaGraphicsResource* positionsVBO_CUDA);
+
+void cudaSetDeviceWrapper(int n);
+
+#endif
---
+++ BoidSim/BoidSim/spatial_hash.hpp
@@ -0,0 +1,15 @@
+#ifndef spatial_hash_hpp
+#define spatial_hash_hpp 
+
+#include <iostream>
+#include "boid.h"
+
+void putInHashTable(Boid& b);
+void clearHashTable();
+std::vector<Boid*> getNeighbours(Boid& b);
+inline bool validNeighbour(Boid& a, Boid& b);
+
+extern const int nrBoids;
+extern std::vector<Boid> boids;
+
+#endif
